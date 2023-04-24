0c926ca424651b144e27f36bb92999def8b68a1c
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-04-24 17:18:03 +0900

    ローカルの時のlisen修正

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
     logger: new MyLogger(),
   });
   const port = Number(process.env.PORT) || 4000; // Cloud Run の要件。環境変数PORTで起動するようにする。
-  await app.listen(port, '0.0.0.0'); // '0.0.0.0' を追加して外部からのアクセスを受け入れる。
+  if ((process.env.NOTE_ENV = 'dev')) {
+    await app.listen(4000, '127.0.0.1');
+  } else {
+    await app.listen(port, '0.0.0.0'); // '0.0.0.0' を追加して外部からのアクセスを受け入れる。
+  }
 }
 bootstrap();
