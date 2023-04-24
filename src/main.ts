9903c6e6c9f31249ac678911493a99e081e63994
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-04-24 18:05:49 +0900

    分岐しなくてもいけたので修正取り消し

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,6 @@ async function bootstrap() {
     logger: new MyLogger(),
   });
   const port = Number(process.env.PORT) || 4000; // Cloud Run の要件。環境変数PORTで起動するようにする。
-  if ((process.env.NOTE_ENV = 'dev')) {
-    await app.listen(port, '127.0.0.1');
-    return;
-  }
   await app.listen(port, '0.0.0.0'); // '0.0.0.0' を追加して外部からのアクセスを受け入れる。
 }
 bootstrap();
