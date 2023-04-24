811bad08320f0ecd2962dd3829503805dfef7582
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-04-24 17:51:56 +0900

    app.listenの呪文再チャレンジ

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,10 @@ async function bootstrap() {
     logger: new MyLogger(),
   });
   const port = Number(process.env.PORT) || 4000; // Cloud Run の要件。環境変数PORTで起動するようにする。
-  await app.listen(
-    port,
-    (process.env.NOTE_ENV = 'dev' ? '127.0.0.1' : '0.0.0.0'),
-  ); // '0.0.0.0' を追加して外部からのアクセスを受け入れる。
+  if ((process.env.NOTE_ENV = 'dev')) {
+    await app.listen(port, '127.0.0.1');
+    return;
+  }
+  await app.listen(port, '0.0.0.0'); // '0.0.0.0' を追加して外部からのアクセスを受け入れる。
 }
 bootstrap();
