31aa828ed45ba1bae5323f05cba8ea8432d627e2
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-05-21 04:09:19 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new MyLogger(),
   });
-  app.enableCors({
-    origin: process.env.REDIRECT_URL,
-    credentials: true,
-  });
   const port = Number(process.env.PORT) || 4000; // Cloud Run の要件。環境変数PORTで起動するようにする。
   await app.listen(port, '0.0.0.0'); // '0.0.0.0' を追加して外部からのアクセスを受け入れる。
 }
