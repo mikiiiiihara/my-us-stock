4d3b11067cff398b14a8a432985bbb67e0c43633
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-05-21 05:00:39 +0900

    cookie設定追加

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MyLogger } from './logger/logger.service';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,6 +11,7 @@ async function bootstrap() {
     origin: 'https://my-us-stock-front.vercel.app',
     credentials: true,
   });
+  app.use(cookieParser());
   const port = Number(process.env.PORT) || 4000; // Cloud Run の要件。環境変数PORTで起動するようにする。
   await app.listen(port, '0.0.0.0'); // '0.0.0.0' を追加して外部からのアクセスを受け入れる。
 }
