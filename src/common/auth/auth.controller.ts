337085fc1bf4842ee6feb472e9ab1510cd9fb73d
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-05-21 16:18:38 +0900

    Update auth.controller.ts

--- src/common/auth/auth.controller.ts
+++ src/common/auth/auth.controller.ts
@@ -28,13 +28,11 @@ export class AuthController {
       httpOnly: true,
       sameSite: process.env.NODE_ENV !== 'dev' ? 'none' : undefined,
       secure: process.env.NODE_ENV !== 'dev',
-      domain: process.env.COOKIE_DOMAIN ?? 'localhost',
     });
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       sameSite: process.env.NODE_ENV !== 'dev' ? 'none' : undefined,
       secure: process.env.NODE_ENV !== 'dev',
-      domain: process.env.COOKIE_DOMAIN ?? 'localhost',
     });
     const baseUrl = this.configService.get<string>('REDIRECT_URL');
     res.redirect(`${baseUrl}/home`);
@@ -58,13 +56,11 @@ export class AuthController {
       httpOnly: true,
       sameSite: process.env.NODE_ENV !== 'dev' ? 'none' : undefined,
       secure: process.env.NODE_ENV !== 'dev',
-      domain: process.env.COOKIE_DOMAIN ?? 'localhost',
     });
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       sameSite: process.env.NODE_ENV !== 'dev' ? 'none' : undefined,
       secure: process.env.NODE_ENV !== 'dev',
-      domain: process.env.COOKIE_DOMAIN ?? 'localhost',
     });
     return { accessToken };
   }
