c78b1cfa9dcb419264a8741453a5073cf175999a
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-05-21 17:09:32 +0900

    bearer-token形式取りやめ

--- src/common/auth/strategies/access-token.strategy.ts
+++ src/common/auth/strategies/access-token.strategy.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
 
 type JwtPayload = {
   sub: string;
@@ -12,7 +13,13 @@ type JwtPayload = {
 export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(private configService: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: (req: Request) => {
+        let token = null;
+        if (req && req.cookies) {
+          token = req.cookies['accessToken'];
+        }
+        return token;
+      },
       secretOrKey: configService.get<string>('jwt.accessSecret'),
     });
   }
