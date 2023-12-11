94a7ec5bcb44f8a67e71610cd1234c5d8fdf3c6c
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-12-11 12:23:21 +0900

    veresion.json検証スタート

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import * as path from 'path';
+import * as fs from 'fs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    // __dirnameを使用して現在のディレクトリのパスを取得し、version.jsonのパスを組み立てる
+    const versionFilePath = path.join(__dirname, 'version.json');
+
+    // ファイルの存在を確認
+    if (!fs.existsSync(versionFilePath)) {
+      return 'OK';
+    }
+
+    try {
+      const versionInfo = fs.readFileSync(versionFilePath, 'utf8');
+      return JSON.parse(versionInfo);
+    } catch (error) {
+      // ファイルの読み込み中にエラーが発生した場合
+      return 'OK';
+    }
   }
 }
